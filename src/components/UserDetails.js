2a34169a18afa8ebf7ac925c921efd9bcdb9e0a2
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-31 00:33:17 +0530

    Show user details in table and style it

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,23 +1,51 @@
 import React from 'react';
+import { Table, TableRow, TableCell, TableBody, Paper, Typography } from '@material-ui/core'
 import { getUserById } from '../services/Users';
+
+const shownFields = [
+  { key: 'company_name', fieldName: 'Company' },
+  { key: 'city', fieldName: 'City' },
+  { key: 'state', fieldName: 'State' },
+  { key: 'zip', fieldName: 'Zip' },
+  { key: 'email', fieldName: 'Email' },
+  { key: 'web', fieldName: 'Web' },
+  { key: 'age', fieldName: 'Age' }
+];
+
+const styles = {
+  root: {
+    maxWidth: '700px',
+    margin: '3em auto',
+    padding: '0.1em',
+  },
+  title: {
+    margin: '1em',
+  },
+  fieldHeading: {
+    fontSize: '0.9em',
+  }
+};
 
 const UserDetails = ({ match }) => {
   const userId = Number(match.params.userId);
   const user = getUserById(userId);
 
   return (
-    <div className="UserDetails">
-      <h2>User Details : userId</h2>
-      <p>{user.first_name}</p>
-      <p>{user.last_name}</p>
-      <p>{user.company_name}</p>
-      <p>{user.city}</p>
-      <p>{user.state}</p>
-      <p>{user.zip}</p>
-      <p>{user.email}</p>
-      <p>{user.web}</p>
-      <p>{user.age}</p>
-    </div>
+    <Paper style={styles.root}>
+      <Typography style={styles.title} variant="headline" component="h2">
+        {user.first_name} {user.last_name}
+      </Typography>
+      <Table>
+        <TableBody>
+          {shownFields.map(({ key, fieldName }) =>
+            <TableRow id={key}>
+              <TableCell style={styles.fieldHeading} component="th" scope="row"> {fieldName} </TableCell>
+              <TableCell numeric> {user[key]} </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 };
 
