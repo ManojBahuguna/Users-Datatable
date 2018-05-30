6129f3a115e233b42c005a602570404231485668
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-30 23:30:33 +0530

    Get user details by id

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { getUserById } from '../services/Users';
 
-const UserDetails = ({ match }) => (
-  <div className="UserDetails">
-    User Details : {match.params.userId}
-  </div>
-);
+const UserDetails = ({ match }) => {
+  const userId = Number(match.params.userId);
+  const user = getUserById(userId);
+
+  return (
+    <div className="UserDetails">
+      <h2>User Details : userId</h2>
+      <p>{user.first_name}</p>
+      <p>{user.last_name}</p>
+      <p>{user.company_name}</p>
+      <p>{user.city}</p>
+      <p>{user.state}</p>
+      <p>{user.zip}</p>
+      <p>{user.email}</p>
+      <p>{user.web}</p>
+      <p>{user.age}</p>
+    </div>
+  );
+};
 
 export default UserDetails;
--- src/services/Users.js
+++ src/services/Users.js
@@ -1,9 +1,9 @@
 export const API_URL = 'http://demo9197058.mockable.io/users';
 
-// original Data of all the users
+// original data of all the users
 let users = [];
 
-// fetch users from server if not fetched yet and return users.
+// fetch users from server.
 export const fetchUsers = async () => {
   await fetch(API_URL)
     .then(res => res.json())
@@ -15,3 +15,9 @@ export const fetchUsers = async () => {
   // return the users
   return users;
 };
+
+
+export const getUserById = userId =>
+  users.find(user =>
+    user.id === userId
+  );
