a2bb74300e81ebc42ab030046f67d2c3c5e019de
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-30 21:10:31 +0530

    Project Setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="UsersDataApp">
+        Users Data App
+      </div>
+    );
+  }
+}
+
+export default App;
